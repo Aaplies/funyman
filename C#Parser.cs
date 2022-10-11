aaf6732ebc427e19e26ac9c3cb8c71969514accd
iamSkev <shawnkenzov@gmail.com>
2022-10-11 23:41:04 +0900

    Fixed the indentation

--- C#Parser.cs
+++ C#Parser.cs
@@ -6,34 +6,32 @@ namespace Duck
 	{
 		public static long Main(string s)
 		{
+            Dictionary<string, long> suffixes = new Dictionary<string, long>()
             {
-                Dictionary<string, long> suffixes = new Dictionary<string, long>()
-                {
-                    {"k", 1000 },
-                    {"m", 1_000_000 },
-                    {"b", 1_000_000_000 },
-                    {"t", 1_000_000_000_000 },
-                    {"qa", 1_000_000_000_000_000 },
-                    {"qi", 1_000_000_000_000_000_000 }
-                };
-                if (s.Contains(","))
-                {
-                    s = (new Regex(",")).Replace(s, "");
-                }
-                if (new Regex(@"[a-zA-Z]").Match(s).Length != 0)
-                {
-                    Regex rg_d = new Regex(@"[a-zA-Z]");
-                    Regex rg_l = new Regex(@"[^a-zA-Z]\.?");
-                    string letter = rg_l.Replace(s, "");
-                    double digit = double.Parse(rg_d.Replace(s, ""));
-                    double result = digit * (suffixes[letter]);
-                    return Convert.ToInt64(result);
-                }
-                else
-                {
-                    long result = long.Parse(s);
-                    return Convert.ToInt64(result);
-                }
+                {"k", 1000 },
+                {"m", 1_000_000 },
+                {"b", 1_000_000_000 },
+                {"t", 1_000_000_000_000 },
+                {"qa", 1_000_000_000_000_000 },
+                {"qi", 1_000_000_000_000_000_000 }
+            };
+            if (s.Contains(","))
+            {
+                s = (new Regex(",")).Replace(s, "");
+            }
+            if (new Regex(@"[a-zA-Z]").Match(s).Length != 0)
+            {
+                Regex rg_d = new Regex(@"[a-zA-Z]");
+                Regex rg_l = new Regex(@"[^a-zA-Z]\.?");
+                string letter = rg_l.Replace(s, "");
+                double digit = double.Parse(rg_d.Replace(s, ""));
+                double result = digit * (suffixes[letter]);
+                return Convert.ToInt64(result);
+            }
+            else
+            {
+                long result = long.Parse(s);
+                return Convert.ToInt64(result);
             }
         }
 	}
