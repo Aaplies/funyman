dbd0fab56708ff7527344c42533276157835e3a6
Aaplies <74510508+Aaplies@users.noreply.github.com>
2023-04-05 13:42:50 -0500

    updated ver
    no suffixes or timeframe, but is up-to-date with SPTS: Endless :+1:

--- tokenmaximizer.cpp
+++ tokenmaximizer.cpp
@@ -1,138 +1,67 @@
-/* 
-300 is 4,500
-100 is 1,620
-1 day is 99
-1 week is 499
-*/
 #include <iostream>
 #include <vector>
 
-std::string add_commas(const std::string& input)
-{
-    auto s = input;
-
-   int n = s.length() - 3;
-   int end = (std::stol(input) >= 0) ? 0 : 1; // Support for negative numbers
-   while (n > end) {
-      s.insert(n, ",");
-      n -= 3;
-   }
-   return s;
-}
-
-long simulate_tokens(const long& tokens, const long& tpm, const float& days)
-{
-    long simulated_tokens, simulated_tpm;
-    simulated_tpm = tpm;
-    simulated_tokens = tokens;
-    for (int i = 1; i <= days * 1440; i++)
-    {
-        if (i % 240 == 0)
-        {
-            simulated_tpm++;
-        }
-        simulated_tokens += simulated_tpm;
-    }
-    return simulated_tokens;
-}
-
 int main()
 {
-    std::vector<int> best = {0, 0, 0, 0};
-    long utokens, tokens, utpm, tpm, budget, spent, besttokens, iterations, bestspent, besttpm, oldbest, betteroldbest = 0;
-    int maxtpmlength, mintpmlength, weeklength, daylength; // increase if you reach a cap, will make code slower
-    float days = 0;
-    std::cout << "What time frame (days): ";
-    std::cin >> days;
-    std::cout << "How many tokens: ";
-    std::cin >> utokens;
-    std::cout << "What tpm: ";
-    std::cin >> utpm;
-    std::cout << "What's your budget: ";
+    double tokens, tpm, usertokens, usertpm, spent, bestspent, budget, besttokens, days = 0;
+    std::vector<int> best = {0, 0, 0, 0, 0}; // 300, 3000, week, month, year
+    int Mlength, yearlength, length, weeklength, monthlength, mlength, iterations = 0;
+    std::cout << "TPM: ";
+    std::cin >> usertpm;
+    std::cout << "Tokens: ";
+    std::cin >> usertokens;
+    std::cout << "Budget: ";
     std::cin >> budget;
-    maxtpmlength = int(budget / 4500) + 1;
-    mintpmlength = 10;
-    weeklength = int(budget / 499) + 1;
-    daylength = 10;
-    long length = maxtpmlength;
-    length *= mintpmlength;
-    length *= weeklength;
-    length *= daylength;
-    length /= 20;
-    // days = (days == 0) ? 1 : days;
-    if (budget >= 1000000)
+    Mlength = int(budget / 4500) + 1;
+    yearlength = int(budget / 9999) + 1;
+    for (int mTPM = 0; mTPM <= 7; mTPM++)
     {
-        std::cout << "\nAny number between 1-5 million will take a while. \nPlease do not complain about how slow it takes to do 10 million.\n";
-        std::cout << "Progress messages turned on cause this'll take longer than usual\n\n";
-    }
-    if (budget >= 10000000)
-    {
-        std::cout << "\nTBH just be f2p (anything this big will break the code)\n";
-        return 1;
-    }
-    for (int maxtpm = 0; maxtpm <= maxtpmlength; maxtpm++)
-    {
-        for (int mintpm = 0; mintpm <= mintpmlength; mintpm++)
+        for (int MTPM = 0; MTPM <= Mlength; MTPM++)
         {
-            for (int week = 0; week <= weeklength; week++)
+            for (int month = 0; month <= 7; month++)
             {
-                for (int day = 0; day <= daylength; day++)
+                for (int week = 0; week <= 4; week++)
                 {
-                    if (besttokens - oldbest == 0 && day != 0 && budget > 1000)
-                    {
-                        continue;
-                    }
-                    iterations++;
-                    if (iterations % length == 0)
-                    {
-                        if (!((iterations / length) * 5 >= 100) && days >= 1 && budget >= 100000)
+                    for (int year = 0; year <= yearlength; year++)
+                    {   
+                        iterations++;
+                        spent = mTPM * 4500;
+                        spent += MTPM * 30000;
+                        spent += month * 1499;
+                        spent += year * 9999;
+                        spent += week * 499;
+                        if (spent > budget)
                         {
-                            std::cout << "You're " << (iterations / length) * 5 << "% there! (" << add_commas(std::to_string(besttokens - oldbest)) << " improvement)\n";
-                            oldbest = besttokens;
+                            continue;
                         }
-                    }
-                    spent = maxtpm * 4500;
-                    spent += mintpm * 1620;
-                    spent += week * 499;
-                    spent += day * 99;
-                    if (spent > budget)
-                    {
-                        continue;
-                    }
-                    if (weeklength - 1 == week)
-                    {
-                        weeklength++;
-                    }
-                    if (maxtpmlength - 1 == maxtpmlength)
-                    {
-                        maxtpmlength++;
-                    }
-                    tokens = utokens;
-                    tpm = utpm;
-                    tpm += maxtpm * 300;
-                    tpm += mintpm * 100;
-                    tokens += tpm * 10080 * week;
-                    tokens += tpm * 1440 * day;
-                    // now test if better than before
-                    if (simulate_tokens(tokens, tpm, days) > besttokens)
-                    {
-                        besttokens = simulate_tokens(tokens, tpm, days);
-                        bestspent = spent;
-                        besttpm = tpm;
-                        best[0] = maxtpm;
-                        best[1] = mintpm;
-                        best[2] = day;
-                        best[3] = week;
+                        tpm = usertpm;
+                        tpm += mTPM * 300;
+                        tpm += MTPM * 3000;
+                        tokens = usertokens;
+                        tokens += tpm * week * 10080;
+                        tokens += tpm * month * 43800;
+                        tokens += tpm * year * 525600;
+                        if (tokens > besttokens)
+                        {
+                            besttokens = tokens;
+                            bestspent = spent;
+                            best[0] = mTPM;
+                            best[1] = MTPM;
+                            best[2] = week;
+                            best[3] = month;
+                            best[4] = year;
+                        }
                     }
                 }
             }
         }
     }
-    std::cout << "\n\n300 tpm : " << add_commas(std::to_string(best[0])) << " (" << add_commas(std::to_string(best[0] * 4500)) << " R$)" << std::endl;
-    std::cout << "100 tpm : " << add_commas(std::to_string(best[1])) << " (" << add_commas(std::to_string(best[1] * 1620)) << " R$)" << std::endl;
-    std::cout << "1 day rush : " << add_commas(std::to_string(best[2])) << " (" << add_commas(std::to_string(best[2] * 99)) << " R$)" <<  std::endl;
-    std::cout << "1 week rush : " << add_commas(std::to_string(best[3])) << " (" << add_commas(std::to_string(best[3] * 499)) << " R$)" << std::scientific;
-    std::cout << "\nYou will get ~" << add_commas(std::to_string(besttokens)) << " tokens with " << add_commas(std::to_string(besttpm + int(days * 6))) << " TPM.\n";
-    std::cout << "You will have " << budget - bestspent << " R$ left you p2w bozo\n\n";
-    std::cout << add_commas(std::to_string(iterations)) << " possible combinations considered.\nAlways be sure to buy TPM before Token Rush.\n";
+    std::cout << "\n300 tpm : " << best[0] << "\n";
+    std::cout << "3000 tpm : " << best[1] << "\n";
+    std::cout << "1 week rush : " << best[2] << "\n";
+    std::cout << "1 month rush : " << best[3] << "\n";
+    std::cout << "1 year rush : " << best[4] << "\n";
+    std::cout << float(besttokens) << " tokens and " << budget - bestspent << " R$ left\n";
+    std::cout << "\n" << float(iterations) << " combos considered.\n";
+    return 0;
 }
