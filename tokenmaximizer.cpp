f76220d5448138dc6197ea3a0efdfad4045de5f4
Aaplies <74510508+Aaplies@users.noreply.github.com>
2023-04-06 13:31:58 -0500

    idk even
    what

--- tokenmaximizer.cpp
+++ tokenmaximizer.cpp
@@ -3,13 +3,12 @@
 
 int main()
 {
-    double tokens, tpm, usertokens, usertpm, spent, bestspent, budget, besttokens, days = 0;
+    double tokens, tpm, usertpm, spent, bestspent, budget, besttokens, days = 0;
     std::vector<int> best = {0, 0, 0, 0, 0}; // 300, 3000, week, month, year
-    int Mlength, yearlength, length, weeklength, monthlength, mlength, iterations = 0;
+    int Mlength, yearlength, length, weeklength, monthlength, mlength = 0;
+    long iterations;
     std::cout << "TPM: ";
     std::cin >> usertpm;
-    std::cout << "Tokens: ";
-    std::cin >> usertokens;
     std::cout << "Budget: ";
     std::cin >> budget;
     Mlength = int(budget / 4500) + 1;
@@ -41,7 +40,7 @@ int main()
                             tpm += mTPM * 300;
                             tpm += MTPM * 3000;
                             tpm += t100 * 100;
-                            tokens = usertokens;
+                            tokens = 0;
                             tokens += tpm * week * 10080;
                             tokens += tpm * month * 43800;
                             tokens += tpm * year * 525600;
@@ -62,13 +61,14 @@ int main()
             }
         }
     }
-    std::cout << "\n300 tpm : " << best[0] << "\n";
+    std::cout << "\n100 tpm : " << best[5] << "\n";
+    std::cout << "300 tpm : " << best[0] << "\n";
     std::cout << "3000 tpm : " << best[1] << "\n";
-    std::cout << "1 week rush : " << best[2] << "\n";
+    std::cout << "\n1 week rush : " << best[2] << "\n";
     std::cout << "1 month rush : " << best[3] << "\n";
     std::cout << "1 year rush : " << best[4] << "\n";
-    std::cout << "100 tpm : " << best[5] << "\n";
     std::cout << float(besttokens) << " tokens and " << budget - bestspent << " R$ left\n";
     std::cout << "\n" << float(iterations) << " combos considered.\n";
+    std::cout << besttokens / bestspent << " tokens per 1 R$\n";
     return 0;
 }
