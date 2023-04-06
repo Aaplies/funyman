e76b14a9a5d8bc74a2a44964d7c8435665812771
Aaplies <74510508+Aaplies@users.noreply.github.com>
2023-04-05 22:32:00 -0500

    added 100 tpm

--- tokenmaximizer.cpp
+++ tokenmaximizer.cpp
@@ -23,33 +23,39 @@ int main()
                 for (int week = 0; week <= 4; week++)
                 {
                     for (int year = 0; year <= yearlength; year++)
-                    {   
-                        iterations++;
-                        spent = mTPM * 4500;
-                        spent += MTPM * 30000;
-                        spent += month * 1499;
-                        spent += year * 9999;
-                        spent += week * 499;
-                        if (spent > budget)
+                    {
+                        for (int t100 = 0; t100 <= 3; t100++)
                         {
-                            continue;
-                        }
-                        tpm = usertpm;
-                        tpm += mTPM * 300;
-                        tpm += MTPM * 3000;
-                        tokens = usertokens;
-                        tokens += tpm * week * 10080;
-                        tokens += tpm * month * 43800;
-                        tokens += tpm * year * 525600;
-                        if (tokens > besttokens)
-                        {
-                            besttokens = tokens;
-                            bestspent = spent;
-                            best[0] = mTPM;
-                            best[1] = MTPM;
-                            best[2] = week;
-                            best[3] = month;
-                            best[4] = year;
+                            iterations++;
+                            spent = mTPM * 4500;
+                            spent += MTPM * 30000;
+                            spent += t100 * 1620;
+                            spent += month * 1499;
+                            spent += year * 9999;
+                            spent += week * 499;
+                            if (spent > budget)
+                            {
+                                continue;
+                            }
+                            tpm = usertpm;
+                            tpm += mTPM * 300;
+                            tpm += MTPM * 3000;
+                            tpm += t100 * 100;
+                            tokens = usertokens;
+                            tokens += tpm * week * 10080;
+                            tokens += tpm * month * 43800;
+                            tokens += tpm * year * 525600;
+                            if (tokens > besttokens)
+                            {
+                                besttokens = tokens;
+                                bestspent = spent;
+                                best[0] = mTPM;
+                                best[1] = MTPM;
+                                best[2] = week;
+                                best[3] = month;
+                                best[4] = year;
+                                best[5] = t100;
+                            }
                         }
                     }
                 }
@@ -61,6 +67,7 @@ int main()
     std::cout << "1 week rush : " << best[2] << "\n";
     std::cout << "1 month rush : " << best[3] << "\n";
     std::cout << "1 year rush : " << best[4] << "\n";
+    std::cout << "100 tpm : " << best[5] << "\n";
     std::cout << float(besttokens) << " tokens and " << budget - bestspent << " R$ left\n";
     std::cout << "\n" << float(iterations) << " combos considered.\n";
     return 0;
